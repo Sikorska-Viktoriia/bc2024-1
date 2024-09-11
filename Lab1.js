d970fa03afdf6eac1fad654cc0244da784a9daab
Sikorska Viktoriia <sikorska.viktoriia@gmail.com>
2024-09-11 16:26:20 +0300

    first commit

---
+++ Lab1.js
@@ -0,0 +1,10 @@
+// Задайте ваше ім'я користувача GitHub тут
+const gitUsername = 'Sikorska-Viktoriia';
+
+// Функція для виведення привітання
+function greet(username) {
+    console.log(`Hello, ${username}`);
+}
+
+// Викликаємо функцію з вашим ім'ям користувача
+greet(gitUsername);
